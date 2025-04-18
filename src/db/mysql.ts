8221b062d55e227e7d95631209e61e2c0be3ca59
GaxCor <gaign1b10@gmail.com>
2025-04-18 16:03:43 -0600

    Update mysql.ts

--- src/db/mysql.ts
+++ src/db/mysql.ts
@@ -1,10 +1,11 @@
 import mysql from "mysql2/promise";
+import "dotenv/config";
 
 export const getConnection = async () => {
   return await mysql.createConnection({
     host: "localhost", // está en la misma instancia
     user: "nachobot",
     password: "supersecreta",
-    database: "P8", // o el nombre que tenga tu base
+    database: process.env.DB_NAME, // o el nombre que tenga tu base
   });
 };
